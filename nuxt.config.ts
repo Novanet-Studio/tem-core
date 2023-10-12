61e25f0bd80a53910b7ba004e52a572199b383f9
David Arenas <davejs136@gmail.com>
2023-10-12 14:50:31 -0400

    add: runtimeConfig and copy emails to parent

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
+import { join } from 'path';
+import { __dirname, copyEmailsDir } from './helper';
+
+copyEmailsDir();
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ['vue-email/nuxt'],
+  imports: {
+    dirs: [join(__dirname, './emails'), join(__dirname, './types')],
+  },
+  runtimeConfig: {
+    sendgrid: {
+      apiKey: process.env.SENDGRID_API_KEY,
+      senderEmail: process.env.SENDGRID_SENDER_MAIL,
+      receiverEmail: process.env.SENDGRID_RECEIVER_MAIL,
+    },
+  },
+});
