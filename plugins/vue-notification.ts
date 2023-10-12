bab6db7bd9285f320ad0d16a4c070669ee3d7b41
David Arenas <davejs136@gmail.com>
2023-10-12 17:01:56 -0400

    feat: configure `vue-notification` plugin

---
+++ plugins/vue-notification.ts
@@ -0,0 +1,11 @@
+import Notification, { notify } from '@kyvg/vue3-notification';
+
+export default defineNuxtPlugin((plugin) => {
+  plugin.vueApp.use(Notification);
+
+  return {
+    provide: {
+      notify,
+    },
+  };
+});
