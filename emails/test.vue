f57a1969c544638e4ff6b7dfb9194276fcfad8ed
David Arenas <davejs136@gmail.com>
2023-10-12 14:49:59 -0400

    add: emails dir

---
+++ emails/test.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps({
+  name: String,
+  lastName: String,
+  age: Number,
+});
+</script>
+
+<template>
+  <EHtml>
+    <EHead />
+    <EPreview>Test email</EPreview>
+    <EBody>
+      <h1>{{ name }} {{ lastName }}</h1>
+      <p>Age: {{ age }}</p>
+    </EBody>
+  </EHtml>
+</template>
