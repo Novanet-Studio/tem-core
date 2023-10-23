1e704db6f58f833ba627ccc052d8b9670712d111
David Arenas <davejs136@gmail.com>
2023-10-23 13:50:11 -0400

    chore: remove vueuse

--- helper.ts
+++ helper.ts
@@ -1,16 +1,4 @@
 import { fileURLToPath } from 'url';
-import { dirname, join, resolve } from 'path';
-import { copySync, existsSync } from 'fs-extra';
+import { dirname } from 'path';
 
 export const __dirname = dirname(fileURLToPath(import.meta.url));
-
-// Copy emails directory to parent directory
-export function copyEmailsDir() {
-  const baseEmailsDir = join(__dirname, './emails');
-  const parentEmailsDir = resolve('./emails');
-
-  if (baseEmailsDir !== parentEmailsDir && !existsSync(parentEmailsDir)) {
-    copySync(baseEmailsDir, parentEmailsDir);
-    console.log('✓ emails copied');
-  }
-}
